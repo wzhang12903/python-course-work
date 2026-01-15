13435d3d0f4ed6a41486d8a49f3fa6e77bb86c88
wei zhang <kbs5921@gmail.com>
2026-01-14 22:04:13 -0500

    first_python_script
    Initial assignment submission - Completed greeting statement and numerical calculations
    Added the first Python script, including printing and basic calculation functions.

---
+++ hello.py
@@ -0,0 +1,18 @@
+# My First Python Program - Coursework
+
+print("Hello, Python! My name is Wei Zhang and I am learning Python.")
+
+num1 = 14
+num2 = 3
+result = num1 * num2
+
+print(f"The result of {num1} multiplied by {num2} is {result}")
+
+add_num1 = 20
+add_num2 = 22
+add_result = add_num1 + add_num2
+print(f"The result of {add_num1} added to {add_num2} is {add_result}")
+
+
+
+
